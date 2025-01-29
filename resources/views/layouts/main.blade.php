1091a3c75d6fdd689315fd281867dbf1d9df80df
Si Thu Zaw <sithuzaw200258@gmail.com>
2025-01-29 15:55:44 +0630

    Add Search Dropdown Feature / Livewire

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Movies App</title>
     @vite('resources/css/app.css')
+    @livewireStyles
 </head>
 
 <body class="font-sans bg-gray-900 text-white">
@@ -28,12 +29,10 @@
                 </li>
             </ul>
             <div class="flex flex-col md:flex-row items-center">
-                <div class="relative mt-3 md:mt-0">
-                    <input type="text" class="bg-gray-800 text-sm rounded-full w-64 px-4 pl-8 py-1 focus:outline-none focus:shadow-outline" placeholder="Search">
-                    <div class="absolute top-0">
-                        <svg class="fill-current w-4 text-gray-500 mt-2 ml-2" viewBox="0 0 24 24"><path class="heroicon-ui" d="M16.32 14.9l5.39 5.4a1 1 0 01-1.42 1.4l-5.38-5.38a8 8 0 111.41-1.41zM10 16a6 6 0 100-12 6 6 0 000 12z"/></svg>
-                    </div>
-                </div>
+                {{-- search movies --}}
+                <livewire-search-dropdown />
+
+                {{-- user avatar --}}
                 <div class="md:ml-4 mt-3 md:mt-0">
                     <a href="#">
                         <img src="/img/avatar.jpg" alt="avatar" class="rounded-full w-8 h-8">
@@ -43,6 +42,7 @@
         </div>
     </nav>
     @yield('content')
+    @livewireScripts
 </body>
 
 </html>
